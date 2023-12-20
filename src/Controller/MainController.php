fec995f45b96ca863ab37b6de4b1866b8c4f910f
hakimo <achache.hakim@gmail.com>
2023-12-20 14:28:07 +0100

    change page home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(UsersRepository $users): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'users' => $users->findAll(),
         ]);
     }
     #[Route('/contact', name: 'app_contact')]
